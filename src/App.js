e48a1249fef9a6a219d2852b8fc38dd0bcc6333a
biancabera <biancabera@gmail.com>
2022-08-22 17:35:09 -0300

    Requisitos 17 e 18

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Drinks from './pages/Drinks';
 import Foods from './pages/Foods';
+import Profile from './pages/Profile';
 import './App.css';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/foods" component={ Foods } />
-      <Route path="/drinks" component={ Drinks } />
-      <Route path="/foods/{id-da-receita}" />
-      <Route path="/drinks/{id-da-receita}" />
-      <Route path="/foods/{id-da-receita}/in-progress" />
-      <Route path="/drinks/{id-da-receita}/in-progress" />
-      <Route path="/profile" />
+      <Route exact path="/foods" component={ Foods } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/foods/{id-da-receita}" />
+      <Route exact path="/drinks/{id-da-receita}" />
+      <Route exact path="/foods/{id-da-receita}/in-progress" />
+      <Route exact path="/drinks/{id-da-receita}/in-progress" />
+      <Route path="/profile" component={ Profile } />
       <Route path="/done-recipes" />
       <Route path="/favorite-recipes" />
     </Switch>
