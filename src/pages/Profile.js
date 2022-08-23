df2ad7f96d7f1a41fdc0034efc851dc91ab0e7c1
biancabera <biancabera@gmail.com>
2022-08-22 22:00:22 -0300

    Requisito 57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
 
@@ -10,6 +11,14 @@ function Profile() {
   return (
     <div>
       <Header />
+      <p data-testid="profile-email" />
+      <Link to="/done-recipes">
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      </Link>
+      <Link to="/favorites">
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      </Link>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
     </div>
   );
 }
