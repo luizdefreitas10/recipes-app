45e8e988648afe1fb546cf56d682288ed7f4f5d2
VictorSilva27 <victoradaosilva2@gmail.com>
2022-08-23 14:34:17 -0300

    arrumando 17

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
       <Route exact path="/" component={ Login } />
       <Route exact path="/foods" component={ Foods } />
       <Route exact path="/drinks" component={ Drinks } />
-      <Route exact path="/foods/{id-da-receita}" />
-      <Route exact path="/drinks/{id-da-receita}" />
-      <Route exact path="/foods/{id-da-receita}/in-progress" />
-      <Route exact path="/drinks/{id-da-receita}/in-progress" />
+      <Route path="/foods/{id-da-receita}" />
+      <Route path="/drinks/{id-da-receita}" />
+      <Route path="/foods/{id-da-receita}/in-progress" />
+      <Route path="/drinks/{id-da-receita}/in-progress" />
       <Route path="/profile" component={ Profile } />
       <Route path="/done-recipes" />
       <Route path="/favorite-recipes" />
