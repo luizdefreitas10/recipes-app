2fbf032dda6ba56bbc2703716c50e7ac3788b499
biancabera <biancabera@gmail.com>
2022-08-23 10:38:16 -0300

    Requisito 58

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,23 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
 
 function Profile() {
+  const [email, setEmail] = useState('');
+  const emailStorage = () => {
+    const getEmail = JSON.parse(localStorage.getItem('user'));
+    setEmail(getEmail.email);
+  };
   const { setTitlePage } = useContext(RecipesContext);
   useEffect(() => {
+    emailStorage();
     setTitlePage('Profile');
   }, [setTitlePage]);
   return (
     <div>
       <Header />
-      <p data-testid="profile-email" />
+      <p data-testid="profile-email">{ email }</p>
       <Link to="/done-recipes">
         <button type="button" data-testid="profile-done-btn">Done Recipes</button>
       </Link>
