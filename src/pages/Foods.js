371b7360a489415ff30bfc5a973e2e4feb2ce8da
VictorSilva27 <victoradaosilva2@gmail.com>
2022-08-23 14:39:05 -0300

    Arrumando Header

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 function Foods() {
+  const { setTitlePage } = useContext(RecipesContext);
+  useEffect(() => {
+    setTitlePage('Foods');
+  }, [setTitlePage]);
   return (
     <div>
       <Header />
