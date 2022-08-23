91d3cb583e612b87e2e3b56b7c267145d7e92dfb
thais viana <thaisviana081@outlook.com>
2022-08-23 16:38:02 -0300

    ajust

--- src/SearchFoodDrink.js
+++ src/SearchFoodDrink.js
@@ -8,17 +8,20 @@ export const Foods = (searchA, searchB) => {
   if (searchB === 'Ingredient') {
     const fetchIngredientFood = async () => {
       const results = await fetchApiIngredientFood(searchA);
+      // console.log(results);
     };
     fetchIngredientFood();
   } if (searchB === 'Name') {
     const fetchNameFood = async () => {
       const results = await fetchApiNameFood(searchA);
+      console.log(results);
     };
     fetchNameFood();
   }
   if (searchB === 'First Letter') {
     const fetchFirstLetterFood = async () => {
       const results = await fetchApiFirstLetterFood(searchA);
+      console.log(results);
     };
     fetchFirstLetterFood();
   }
@@ -28,17 +31,20 @@ export const Drinks = (searchA, searchB) => {
   if (searchB === 'Ingredient') {
     const fetchIngredientDrink = async () => {
       const results = await fetchApiIngredientDrink(searchA);
+      console.log(results);
     };
     fetchIngredientDrink();
   } if (searchB === 'Name') {
     const fetchNameDrink = async () => {
       const results = await fetchApiNameDrink(searchA);
+      console.log(results);
     };
     fetchNameDrink();
   }
   if (searchB === 'First Letter') {
     const fetchFirstLetterDrink = async () => {
       const results = await fetchApiFirstLetterDrink(searchA);
+      console.log(results);
     };
     fetchFirstLetterDrink();
   }
