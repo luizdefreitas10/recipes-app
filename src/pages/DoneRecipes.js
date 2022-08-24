79a3ef9ed87be0658c569a3636fef8cec61037f1
VictorSilva27 <victoradaosilva2@gmail.com>
2022-08-24 13:51:37 -0300

    Requisito 44

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,15 +1,70 @@
 import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import RecipesContext from '../context/RecipesContext';
+import ShareIcon from '../images/shareIcon.svg';
 
 function DoneRecipes() {
   const { setTitlePage } = useContext(RecipesContext);
   useEffect(() => {
     setTitlePage('Done Recipes');
   }, [setTitlePage]);
+
+  const referenceData = [
+    {
+      id: '52771',
+      type: 'food',
+      nationality: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+      doneDate: '23/06/2020',
+      tags: ['Pasta', 'Curry'],
+    },
+    {
+      id: '178319',
+      type: 'drink',
+      nationality: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+      doneDate: '23/06/2020',
+      tags: [],
+    },
+  ];
   return (
     <div>
       <Header />
+      <div>
+        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button data-testid="filter-by-food-btn" type="button">Food</button>
+        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+        {referenceData.map((recipe, index) => (
+          <p key={ recipe.id }>
+            <img
+              src={ recipe.image }
+              alt={ recipe.name }
+              data-testid={ `${index}-horizontal-image` }
+            />
+            <p data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</p>
+            <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+            <button type="button">
+              <img
+                src={ ShareIcon }
+                alt="Share Icon"
+                data-testid={ `${index}-horizontal-share-btn` }
+              />
+            </button>
+            {recipe.tags.map((item, indexs) => (
+              <p key={ indexs } data-testid={ `${index}-${item}-horizontal-tag` }>
+                {item}
+              </p>
+            ))}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
