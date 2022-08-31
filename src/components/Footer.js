ddce6530861bb76c01cd07f21a5aa18fdeccc408
biancabera <biancabera@gmail.com>
2022-08-30 22:24:43 -0300

    CSS Footer e Profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import drinkItem from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import './Footer.css';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
+    <footer data-testid="footer" className="footer">
       <Link to="/drinks">
         <img
+          className="drink-footer"
           src={ drinkItem }
           alt="Drink Icon"
           data-testid="drinks-bottom-btn"
@@ -15,6 +17,7 @@ function Footer() {
       </Link>
       <Link to="/foods">
         <img
+          className="food-footer"
           src={ mealIcon }
           alt="Meal Icon"
           data-testid="food-bottom-btn"
